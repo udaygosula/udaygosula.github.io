8c3acd281b75a89b2aa88b9156d25dd3cd7584ce
SATYA UDAY KUMAR GOSULA <satyaudaygosula@gmail.com>
2018-09-08 13:48:43 +0530

    php update
    php updated for mail.

--- php/feed.php
+++ php/feed.php
@@ -3,9 +3,10 @@
 		$name = $_POST['cname'];
 		$email = $_POST['mail'];
 		$feed = $_POST['feed'];
+		$mail = "gprec.capp2018@gmail.com";
 		$headers = 'From: '.$email . "\r\n" .
-					'Reply-To: '.$email . "\r\n";
-		mail("gprec.Cedu@gmail.com","FeedBack",$feed,$headers);
+					'Reply-To: '.$email. "\r\n";
+		mail($mail,"FeedBack",$feed,$headers);
 		include("../html/info.html");
 	}
 	else{
